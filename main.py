f183315c0460795b8f355961cc6608b315472d04
JurassikLizard <51036391+JurassikLizard@users.noreply.github.com>
2024-11-12 18:25:52 +0000

    moved download destination info to vars

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 from aimbot import Aimbot
 from utils import download_file
-from vars import DOWNLOAD_URL
+from vars import DOWNLOAD_URL, DIRNAME, FILENAME
 import os
 
 if __name__ == '__main__':
-    folder = 'yolo/weights'
-    file = 'aimbot.pt'
-    download_file(DOWNLOAD_URL, folder, file)
-    aimbot = Aimbot(os.path.join(folder, file))
+    download_file(DOWNLOAD_URL, DIRNAME, FILENAME)
+    aimbot = Aimbot(os.path.join(DIRNAME, FILENAME))
 
     print('[main] Running aimbot...')
     aimbot.run()
